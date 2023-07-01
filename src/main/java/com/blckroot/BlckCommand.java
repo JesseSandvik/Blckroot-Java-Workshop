31f90ca7dcad808133cfd506cc119b5e5e52c222
Jesse <sandvikjesse@gmail.com>
2023-07-01 14:51:59 -0400

    adding local machine data

--- src/main/java/com/blckroot/BlckCommand.java
+++ src/main/java/com/blckroot/BlckCommand.java
@@ -4,6 +4,24 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "blck")
 public class BlckCommand implements Runnable {
+    @CommandLine.Parameters(index = "0", description = "Executable Name")
+    static String executableName;
+
+    @CommandLine.Spec
+    CommandLine.Model.CommandSpec spec;
+
+    static void PrintStatus() {
+        String[] machineData = {
+                System.getProperty("user.name"),
+                System.getProperty("os.name"),
+                System.getProperty("os.version")
+        };
+
+        for (int i = 0; i < machineData.length; i++) {
+            System.out.printf("[%s]      |      %s",);
+        }
+    }
+
     @Override
     public void run() {
         System.out.println("Hello World!");
